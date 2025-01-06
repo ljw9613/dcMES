94f2b0be38a30ae5b6f92f5ae966a38400c85460
qidao <qidao13@gmail.com>
2025-01-06 10:03:48 +0800

    add

--- dcMes_vue_system/vue.config.js
+++ dcMes_vue_system/vue.config.js
@@ -26,10 +26,12 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
 
-  publicPath: "/dcMes/",
-  // publicPath: "/dcMesCs/",
-  // publicPath: "/dcMesManage/",
-  outputDir: "../dcMes_server/admin",
+  // publicPath: "/dcMes/",
+  // outputDir: "../dcMes_server/admin",
+  publicPath: "/dcMesManage/",
+  outputDir: "../dcMes_server/adminManage",
+    // publicPath: "/dcMesCs/",
+  // outputDir: "../dcMes_server/adminCs",
   assetsDir: "static",
   lintOnSave: process.env.NODE_ENV === "development",
   productionSourceMap: false,
